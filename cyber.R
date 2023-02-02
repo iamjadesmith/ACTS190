7dccac6cfceddc2bf885f13e4c2273540d4c4457
iamjadesmith <jade.smith@drake.edu>
2023-02-02 14:33:10 -0600

    some changes

--- cyber.R
+++ cyber.R
@@ -2,3 +2,10 @@ rm(list = ls())
 
 cyber <- read.csv("cases.csv", sep = "|")
 str(cyber)
+summary(cyber)
+
+# Data Cleaning ----
+
+cyber <- subset(cyber, select = -c(STREET_ADDR_LINE_2))
+
+is.na(cyber)
