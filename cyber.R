7e4a386aede97772e70457bf61366f0e0225f816
iamjadesmith <jade.smith@drake.edu>
2023-02-07 14:14:43 -0600

    removed some columns

--- cyber.R
+++ cyber.R
@@ -12,7 +12,7 @@ cyber <- subset(cyber, select = -c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPA
                                    FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
                                    CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER, 
                                    JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM, 
-                                   AFFECTED_COUNT, SETTLEMENT_AMOUNT))
+                                   AFFECTED_COUNT, STREET_ADDR_LINE_1, STREET_ADDR_LINE_2, ZIP, COMPANY_ID, MSCAD_IDstr))
 
 #removes columns if there are more than 100,000 NAs in the column
 cyber <- cyber[,colSums(is.na(cyber))<100000]
