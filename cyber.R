392d70ab39b6243e915272d78ee6c33a87e2764d
iamjadesmith <jade.smith@drake.edu>
2023-02-08 09:22:28 -0600

    Fixed the Columns that we would remove

--- cyber.R
+++ cyber.R
@@ -1,22 +1,21 @@
 rm(list = ls())
 
 cyber <- read.csv("cases.csv", sep = "|")
-str(cyber)
-summary(cyber)
 
 # Data Cleaning ----
 
 #Removing Columns Ben didn't like
-cyber <- subset(cyber, select = -c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
+cyber <- subset(cyber, select = c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
                                    CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS, 
                                    FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
                                    CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER, 
                                    JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM, 
-                                   AFFECTED_COUNT, STREET_ADDR_LINE_1, STREET_ADDR_LINE_2, ZIP, COMPANY_ID, MSCAD_IDstr))
+                                   AFFECTED_COUNT, SETTLEMENT_AMOUNT))
 
 #removes columns if there are more than 100,000 NAs in the column
-cyber <- cyber[,colSums(is.na(cyber))<100000]
+# cyber <- cyber[,colSums(is.na(cyber))<100000]
 
 for (x in 1:ncol(cyber)) {
   print(sum(is.na(cyber[,x])))
 }
+
