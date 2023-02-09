df9ce233b6f3160234e4af26755a5eb44ba825f2
iamjadesmith <jade.smith@drake.edu>
2023-02-09 11:08:23 -0600

    comments

--- cyber.R
+++ cyber.R
@@ -4,7 +4,7 @@ cyber <- read.csv("cases.csv", sep = "|")
 
 # Data Cleaning ----
 
-#Removing Columns Ben didn't like
+#Keeping only the columns that we want
 cyber <- subset(cyber, select = c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
                                    CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS, 
                                    FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
