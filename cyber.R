10feae54d1b630252eb7a1b953699a079246b285
iamjadesmith <jade.smith@drake.edu>
2023-02-14 14:51:28 -0600

    selected columns from both datasets

--- cyber.R
+++ cyber.R
@@ -2,16 +2,21 @@ rm(list = ls())
 
 cases <- read.csv("cases.csv", sep = "|")
 
+# Keeping only the columns that we want
+cases <- subset(cases, select = c(COMPANY_ID, ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
+                                  CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS,
+                                  FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER,
+                                  CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER,
+                                  JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM,
+                                  AFFECTED_COUNT, SETTLEMENT_AMOUNT, FINANCIAL_DAMAGES_AMT))
+
 # Data Cleaning ----
 
 stats <- read.csv("stats.csv", sep = "|")
 
+# Picking columns from the other data set
+stats <- subset(stats, select = c(COMPANY_ID, COMPANY_STATUS, SIC, EMPLOYEES, REVENUES, NAIC_SECTOR))
+
+
 cyber <- merge(cases, stats, by = "COMPANY_ID")
 
-# Keeping only the columns that we want
-# cyber <- subset(cyber, select = c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
-#                                    CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS, 
-#                                    FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
-#                                    CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER, 
-#                                    JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM, 
-#                                    AFFECTED_COUNT, SETTLEMENT_AMOUNT, FINANCIAL_DAMAGES_AMT))
