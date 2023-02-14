35d4f4c81c3047032f9ac24e6444b68f47647457
iamjadesmith <jade.smith@drake.edu>
2023-02-14 14:35:33 -0600

    Merged the datasets

--- cyber.R
+++ cyber.R
@@ -1,16 +1,17 @@
 rm(list = ls())
 
-cyber <- read.csv("cases.csv", sep = "|")
+cases <- read.csv("cases.csv", sep = "|")
 
 # Data Cleaning ----
 
-#Keeping only the columns that we want
-cyber <- subset(cyber, select = c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
-                                   CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS, 
-                                   FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
-                                   CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER, 
-                                   JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM, 
-                                   AFFECTED_COUNT, SETTLEMENT_AMOUNT, FINANCIAL_DAMAGES_AMT))
+stats <- read.csv("stats.csv", sep = "|")
 
-#removes columns if there are more than 100,000 NAs in the column
-# cyber <- cyber[,colSums(is.na(cyber))<100000]
+cyber <- merge(cases, stats, by = "COMPANY_ID")
+
+# Keeping only the columns that we want
+# cyber <- subset(cyber, select = c(ACD, CREATION_DATE, LAST_MODIFIED_DATE, COMPANY_NAME, COUNTRY_CODE, CASE_CATEGORY,
+#                                    CASE_TYPE, CLASS_COLLECTIVE_ACTION, PRODUCT_SERVICE_INVOLVED, CASESTATUS, 
+#                                    FIRST_NOTICE_DATE,FIRST_NOTICE_DATE_QUALIFIER, ACCIDENT_DATE, ACCIDENT_DATE_QUALIFIER, 
+#                                    CASE_DESCRIPTION, PROXIMATE_CAUSE, SECONDARY_CAUSE, FILING_YEAR, DOCKET_NUMBER, 
+#                                    JURIS_TRIGGER, JURIS_COUNTRY_CODE, PLAINTIFFS_COMPANY, PLAINTIFFS_LAWFIRM, 
+#                                    AFFECTED_COUNT, SETTLEMENT_AMOUNT, FINANCIAL_DAMAGES_AMT))
