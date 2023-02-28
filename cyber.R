fb85583bedbc8755f88e1df47e81b7b98ced6c18
EldrickDossavi <90222206+EldrickDossavi@users.noreply.github.com>
2023-02-28 14:02:32 -0600

    Graphs

--- cyber.R
+++ cyber.R
@@ -1,12 +1,10 @@
 rm(list = ls())
-
-# Import Libraries
-library(rpart)
-library(rpart.plot)
+#load packages
+library(tidyverse)
+library(lubridate)
 library(ggplot2)
-library(pROC)
 library(RColorBrewer)
-library(dplyr)
+
 
 cases <- read.csv("cases.csv", sep = "|")
 
@@ -25,42 +23,66 @@ stats <- read.csv("stats.csv", sep = "|")
 # Picking columns from the other data set
 stats <- subset(stats, select = c(COMPANY_ID, COMPANY_STATUS, EMPLOYEES, REVENUES, NAIC_SECTOR))
 
+
 cyber <- merge(cases, stats, by = "COMPANY_ID")
 
 # Only keeping the observations that contain a settlement amount
 cyber <- cyber[!is.na(cyber$SETTLEMENT_AMOUNT),]
 
-# remove other columns we don't want
-cyber <- subset(cyber, select = -c(COMPANY_ID, CASE_DESCRIPTION, CASE_CATEGORY, PRODUCT_SERVICE_INVOLVED,
-                                   FIRST_NOTICE_DATE, FIRST_NOTICE_DATE_QUALIFIER, PROXIMATE_CAUSE, 
-                                   SECONDARY_CAUSE, AFFECTED_COUNT))
+View(cyber)
 
-# making variables factors
+summary(cyber)
 
-cyber$CLASS_COLLECTIVE_ACTION <- as.factor(cyber$CLASS_COLLECTIVE_ACTION)
-cyber$COUNTRY_CODE <- as.factor(cyber$COUNTRY_CODE)
-cyber$CASESTATUS <- as.factor(cyber$CASESTATUS)
-cyber$FILING_YEAR <- as.factor(cyber$FILING_YEAR)
-cyber$JURIS_TRIGGER <- as.factor(cyber$JURIS_TRIGGER)
-cyber$JURIS_COUNTRY_CODE <- as.factor(cyber$JURIS_COUNTRY_CODE)
-cyber$COMPANY_STATUS <- as.factor(cyber$COMPANY_STATUS)
-cyber$NAIC_SECTOR <- as.factor(cyber$NAIC_SECTOR)
 
-# Putting Case Type into less factors:
-cyber$CASE_TYPE[cyber$CASE_TYPE %in% c("Data - Malicious Breach", "Data - Physically Lost or Stolen",
-                                       "Data - Unintentional Disclosure")] <- "Data"
-cyber$CASE_TYPE[cyber$CASE_TYPE %in% c("IT - Configuration/Implementation Errors", "IT - Processing Errors")] <- "IT"
-cyber$CASE_TYPE[cyber$CASE_TYPE %in% c("Privacy - Unauthorized Contact or Disclosure",
-                                       "Privacy - Unauthorized Data Collection")] <- "Privacy"
-# cyber$CASE_TYPE <- as.factor(cyber$CASE_TYPE)
 
-# Exploratory Analysis ----
 
-# Plotting Financial Damages Amount
-ggplot(data = cyber) +
-  geom_histogram(aes(x = FINANCIAL_DAMAGES_AMT))
-# Very right skewed. Will try a log transformation
-ggplot(data = cyber) +
-  geom_histogram(aes(x = log(FINANCIAL_DAMAGES_AMT)))
-# This looks much better, will stick with log(FINANCIAL_DAMAGES_AMT)
-cyber$FINANCIAL_DAMAGES_AMT <- log(cyber$FINANCIAL_DAMAGES_AMT)
+# Scatterplot matrix 
+sub = select(cyber, c("REVENUES", "FINANCIAL_DAMAGES_AMT", "SETTLEMENT_AMOUNT", ))
+sub = sub[complete.cases(sub),]
+cor(sub)
+vars = data.frame(sub$REVENUES, sub$FINANCIAL_DAMAGES_AMT, sub$SETTLEMENT_AMOUNT)
+cor(cbind(vars), use = "pairwise.complete.obs")
+pairs(vars, upper.panel=NULL)
+
+#Histograms 
+hist(cyber$REVENUES)
+hist(cyber$FINANCIAL_DAMAGES_AMT)
+hist(cyber$SETTLEMENT_AMOUNT)
+
+cyber %>%
+  ggplot(aes(x = CASE_TYPE)) +
+  ggtitle("Case Type Frequency")+
+  labs(x = "Type of cases", y = "Number of cases ") +
+  geom_bar(fill = "#99d8c9")+
+  coord_flip ()
+
+cyber %>%
+  ggplot(aes(x = CASESTATUS)) +
+  ggtitle("Case Status Frequency")+
+  labs(x = "Status", y = "Count ") +
+  geom_bar(fill = "#99d8c9")+
+  coord_flip ()
+
+cyber %>%
+  ggplot(aes(x = JURIS_TRIGGER)) +
+  ggtitle("Case Status Frequency")+
+  labs(x = "Trigger", y = "Count ") +
+  geom_bar(fill = "#99d8c9")
+
+cyber %>%
+  ggplot(aes(x = NAIC_SECTOR )) +
+  ggtitle("NAIC Sector Frequency")+
+  labs(x = "Sectors", y = "Count ") +
+  geom_bar(fill = "#99d8c9")
+
+
+
+
+
+
+
+
+
+
+
+
